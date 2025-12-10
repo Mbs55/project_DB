79d3ae2c38a45fff526c7eb6a3483bd5793be52e
Mbs <mbs20052005@gmail.com>
2025-12-10 16:56:53 +0100

    connection to db done

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
 import streamlit as st
 import os
 import pandas as pd
+from sqlalchemy import text
+st.title("home page")
+con=st.connection( "mydb",type="sql")
+#con.query("create table user(id integer auto increment not null primary key,name varchar(20) not null")
+with con.session as s:
+    s.execute(text("drop table users if exists"))
+    s.execute(text("create table users(id  int  auto_increment not null primary key,name varchar(25),password varchar(25))"))
+    s.execute(text("insert into users (name,password) values ('mouad','mbs123')"))
+    s.commit()
+
+
+query=con.query("select * from users")
+st.write(query)
+
+
+
 
