c375943bb33f5b73179f1fc60e237c6fb5fcbe39
Mbs <mbs20052005@gmail.com>
2025-12-10 17:47:49 +0100

    update

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ st.title("home page")
 con=st.connection( "mydb",type="sql")
 #con.query("create table user(id integer auto increment not null primary key,name varchar(20) not null")
 with con.session as s:
-    s.execute(text("drop table users if exists"))
+    s.execute(text("drop table if exists users"))
     s.execute(text("create table users(id  int  auto_increment not null primary key,name varchar(25),password varchar(25))"))
     s.execute(text("insert into users (name,password) values ('mouad','mbs123')"))
     s.commit()
